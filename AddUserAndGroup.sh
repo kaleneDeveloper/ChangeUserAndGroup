ba6cf2d8ead371692ef089f4006bf035a025278d
kalene APIOU <apiou.kalene@gmail.com>
2020-01-01 00:39:29 +0100

    complete code oerhaul

--- AddUserAndGroup.sh
+++ AddUserAndGroup.sh
@@ -1,34 +1,51 @@
 #!/bin/bash
-createNewUser() {
-    while true; do
-        read -rp "Do you want create a new user $changeUser ? (y/n): " yn
-        if [[ $yn == @(y|yes) ]]; then
-                sudo adduser "$changeUser"
-                echo "User $changeUser create"
-                break
-        elif [[ $yn == @(n|no) ]]; then break
-        else break
-        fi    
-    done
-}
-checkIfUserExist() {
-getent passwd "$changeUser" &>/dev/null
-}
+changeId() {
 changeUser(){
         read -rp "Enter the name of the new user: " changeUser
         if [ -z "$changeUser" ]; then
             exit 1
         fi
     while true; do
-        if checkIfUserExist; then
+        if getent passwd "$changeUser"; then
             sudo chown "$changeUser" "$file"
             echo "User $changeUser is changed"
             break
         else
             echo "User $changeUser does not exist"
-            createNewUser
+            createNewId
             break
         fi
+    done
+}
+changeGroup(){
+        read -rp "Enter the name of the new group: " changeGroup
+        if [ -z "$changeGroup" ]; then
+            exit 1
+        fi
+    while true; do
+        if getent group "$changeGroup"; then
+            sudo chgrp "$changeGroup" "$file"
+            echo "Group $changeGroup is changed"
+            break
+        else
+            echo "Group $changeGroup does not exist"
+            createNewId
+            break
+        fi
+    done
+}
+}
+createNewId() {
+    createNewUser() {
+    while true; do
+        read -rp "Do you want create a new user $1 ? (y/n): " yn
+        if [[ $yn == @(y|yes) ]]; then
+                sudo adduser "$1"
+                echo "User $1 create"
+                break
+        elif [[ $yn == @(n|no) ]]; then break
+        else break
+        fi    
     done
 }
 createNewGroup() {
@@ -43,26 +60,11 @@ createNewGroup() {
         fi
     done
 }
-checkIfGroupExist() {
-getent passwd "$changeGroup" &>/dev/null
 }
-changeGroup(){
-        read -rp "Enter the name of the new group: " changeGroup
-        if [ -z "$changeGroup" ]; then
-            exit 1
-        fi
-    while true; do
-        if checkIfGroupExist; then
-            sudo chgrp "$changeGroup" "$file"
-            echo "Group $changeGroup is changed"
-            break
-        else
-            echo "Group $changeGroup does not exist"
-            createNewGroup
-            break
-        fi
-    done
-}
+
+
+
+
 while true; do
     read -rp "Enter the name of the file to change: " file
     if [ -z "$file" ]; then
@@ -77,13 +79,10 @@ done
 echo "1. Add user"
 echo "2. Group user"
 while true; do
-    read -rp "Do you want add USER or GROUP: " choose
-    if [ -z "$choose" ]; then
-        exit 1
-    elif [ "$choose" = "1" ] || [ "$choose" = "2" ]; then
-        case $choose in
-            "1") changeUser "$@";;
-            "2") changeGroup "$@";;
-        esac
-    fi
+	read -rp "Do you want add USER or GROUP: " choose
+	case $choose in
+	'') exit 1 ;;
+	'1'|'2') changeId "$choose" ;;
+	*) echo 'Please, choose 1, 2 or leave empty.' ;;
+	esac
 done
